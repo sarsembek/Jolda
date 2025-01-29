60d85c902b209110b8e222655f00c0b2ad86ba0c
sarsembek <ar_sarsembek@kbtu.kz>
2025-01-29 16:01:25 +0500

    fix: debug

--- App.tsx
+++ App.tsx
@@ -1,4 +1,10 @@
-import './global.css';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,11 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
-const { withNativeWind } = require('nativewind/metro');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
- * @type {import('metro-config').MetroConfig}
+ * @type {import('@react-native/metro-config').MetroConfig}
  */
-module.exports = withNativeWind(
-  mergeConfig(getDefaultConfig(__dirname), {
-    // ...existing code...
-  })
-);
+const config = {};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
